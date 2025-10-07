b03b2709558932e5f225bb78b2874e63d4e35a73
Fod9 <dernoncourt.tom@gmail.com>
2025-10-07 09:55:24 +0200

    feat: cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
 from app.routes.game import router as game_router
 from app.services.db_service import DBService
+from fastapi.middleware.cors import CORSMiddleware
+
+# cors settings
+origins = [
+    "*",
+]
 
 db_service = DBService()
 db_service.create_database()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(game_router)
